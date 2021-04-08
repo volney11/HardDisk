44b9fb2a537840972054a557cdfcc355c5b8284b
sysadmin <sysadmin@bcalab1-m08>
2021-04-08 12:03:23 +0530

    this is first commit

---
+++ HardDisk/src/Folder.java
@@ -0,0 +1,15 @@
+public class Folder {
+	String name;
+	int numfiles;
+	Folder (String name, int numfiles)
+	{
+	this.name = name;
+	this.numfiles = numfiles;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
